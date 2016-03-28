ce92698e68323cd461ae2429e7dd7e1e313c49b3
Tiago Paulino <tspaulino@gmail.com>
2016-03-28 19:25:16 -0300

    Validating user_id for images

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   acts_as_taggable
   belongs_to :user
 
-  validates_presence_of :giphy_id
+  validates_presence_of :giphy_id, :user_id
   validates_uniqueness_of :giphy_id, scope: :user_id
 
   def giphy_image
